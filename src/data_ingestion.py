39e370b40321215d26948bf0124958cd1cc536a8
SahilSadaphal <sahilsadaphal8383@gmail.com>
2025-06-01 00:25:14 +0530

    Yaml with params commit

--- src/data_ingestion.py
+++ src/data_ingestion.py
@@ -27,6 +27,24 @@ logger.addHandler(console_handler)
 logger.addHandler(file_handler)
 
 
+def load_params(params_path: str) -> dict:
+    """Load parameters from a YAML file."""
+    try:
+        with open(params_path, "r") as file:
+            params = yaml.safe_load(file)
+        logger.debug("Parameters retrieved from %s", params_path)
+        return params
+    except FileNotFoundError:
+        logger.error("File not found: %s", params_path)
+        raise
+    except yaml.YAMLError as e:
+        logger.error("YAML error: %s", e)
+        raise
+    except Exception as e:
+        logger.error("Unexpected error: %s", e)
+        raise
+
+
 def load_data(data_url: str) -> pd.DataFrame:
     """Load data file from csv file"""
     try:
@@ -73,7 +91,8 @@ def save_data(
 
 def main():
     try:
-        test_size = 0.2
+        params = load_params("params.yaml")
+        test_size = params["data_ingestion"]["test_size"]
         data_path = "https://raw.githubusercontent.com/vikashishere/Datasets/refs/heads/main/spam.csv"
         df = load_data(data_url=data_path)
         final_df = preprocess_data(df=df)
--- src/model_building.py
+++ src/model_building.py
@@ -28,22 +28,23 @@ file_handler.setFormatter(formatter)
 logger.addHandler(console_handler)
 logger.addHandler(file_handler)
 
-# def load_params(params_path: str) -> dict:
-#     """Load parameters from a YAML file."""
-#     try:
-#         with open(params_path, 'r') as file:
-#             params = yaml.safe_load(file)
-#         logger.debug('Parameters retrieved from %s', params_path)
-#         return params
-#     except FileNotFoundError:
-#         logger.error('File not found: %s', params_path)
-#         raise
-#     except yaml.YAMLError as e:
-#         logger.error('YAML error: %s', e)
-#         raise
-#     except Exception as e:
-#         logger.error('Unexpected error: %s', e)
-#         raise
+
+def load_params(params_path: str) -> dict:
+    """Load parameters from a YAML file."""
+    try:
+        with open(params_path, "r") as file:
+            params = yaml.safe_load(file)
+        logger.debug("Parameters retrieved from %s", params_path)
+        return params
+    except FileNotFoundError:
+        logger.error("File not found: %s", params_path)
+        raise
+    except yaml.YAMLError as e:
+        logger.error("YAML error: %s", e)
+        raise
+    except Exception as e:
+        logger.error("Unexpected error: %s", e)
+        raise
 
 
 def load_data(file_path: str) -> pd.DataFrame:
@@ -87,7 +88,8 @@ def train_model(
 
         logger.debug("Initializing RandomForest model with parameters: %s", params)
         clf = RandomForestClassifier(
-            n_estimators=params["n_estimators"], random_state=params["random_state"]
+            n_estimators=params["model_building"]["n_estimators"],
+            random_state=params["model_building"]["random_state"],
         )
 
         logger.debug("Model training started with %d samples", X_train.shape[0])
@@ -127,8 +129,8 @@ def save_model(model, file_path: str) -> None:
 
 def main():
     try:
-        params = {"n_estimators": 25, "random_state": 2}
-        # params = load_params('params.yaml')['model_building']
+        # params = {"n_estimators": 25, "random_state": 2}
+        params = load_params("params.yaml")
         train_data = load_data("./data/processed/train_tfidf.csv")
         X_train = train_data.iloc[:, :-1].values
         y_train = train_data.iloc[:, -1].values
